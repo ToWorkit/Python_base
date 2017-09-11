8811fe0b94783ce3398175e7aa9d43f55b161217
Toworkit <1211126374@qq.com>
2017-09-11 21:38:27 +0800

    0911

---
+++ 模块/內建的sys.py
@@ -0,0 +1,23 @@
+#第1行和第2行是标准注释，第1行注释可以让这个hello.py文件直接在Unix/Linux/Mac上运行，第2行注释表示.py文件本身使用标准UTF-8编码；
+#第4行是一个字符串，表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释；
+#第6行使用__author__变量把作者写进去，这样当你公开源代码后别人就可以瞻仰你的大名；
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'a test module'
+
+__author__ = 'HL'
+
+import sys
+def test():
+  args = sys.argv
+  if len(args) == 1:
+    print('Hello World')
+  elif len(args) == 2:
+    print('Hello, %s' % args[1])  
+  else:
+    print('Too many arguments')
+print(__name__)    
+if __name__ == '__main__':
+  test()
