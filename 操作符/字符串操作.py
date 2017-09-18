9ee7ffe027760a22b9699f2fa568b1a7c1acffc9
Toworkit <1211126374@qq.com>
2017-09-19 07:14:10 +0800

    20170919

--- 操作符/字符串操作.py
+++ 操作符/字符串操作.py
@@ -22,3 +22,39 @@ print('World'.center(20, '*'))
 print('Hello'.startswith('H'))
 print('Hello'.endswith('lo'))
 
+st = 'He\tllo World'
+# 配合 \t 使用
+print(st.expandtabs(tabsize = 10))
+# 查找第一个位置 有则返回位置，无则返回 -1
+print(st.find('\t'))
+
+# 字符串格式化的另一种方式
+l = 'Hello {name}, love {favour}'
+# print(l.format(name = 'Lp', favour = 'pl'))
+print(l.format_map({
+  'name': 'pp',
+  'favour': 'll'
+}))
+# 位置， 有则返回，无则报错
+print(st.index('l'))
+
+# 判断字符串是有否包含数字, 其他字符不行
+print('abc123'.isalnum())
+
+# 去掉换行和空格
+p = '   Hello    '
+print(p)
+print(p.strip())
+
+# 替换, 默认替换所有，第三个参数为控制替换次数(深度)
+r = 'Hello World'
+print(r.replace('World', 'Lp'))
+
+# 字符串分割, 第二个参数为分割深度(次数)
+print(r.split(' '))
+
+sp = '?book_id=90&username=pp'
+print(sp.split('='))
+
+# 首字母都大写
+print('hello world'.title())
