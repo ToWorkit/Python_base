8eb6fc0fbe5481e9dd81caadc3cf496385fbd9f2
Toworkit <1211126374@qq.com>
2017-09-30 21:38:46 +0800

    20170930

--- 01/set.py
+++ 01/set.py
@@ -7,3 +7,11 @@ set_03 = set([1, 1, 2, 2, 3, 4, 4, 5, 6, 6])
 print(set_01 & set_02) # 交集 {2，3}
 print(set_01 | set_02) # 并集 {1, 2, 3, 4}
 print(set_03) # 去重
+s = set([4, 3, 2])
+# add 添加为一个元素
+s.add('abc')
+print(s)
+# update 拆开添加
+s.update('abc')
+s.update([12, '22'])
+print(s)
