8a7c5ea11e89767aea8e1afaf0e6fd15b25b5216
Doing <helong@yuelu.net>
2017-10-06 09:45:52 +0800

    20171006

---
+++ 内建模块/datetime.py
@@ -0,0 +1,6 @@
+# 处理日期和时间
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# 命令行执行
+from datetime import datetime
+print(datetime.now())
--- 正则/re模块.py
+++ 正则/re模块.py
@@ -6,3 +6,29 @@ print(s)
 # r 防止字符串转义
 s2 = r'PL\-LP' 
 print(s2)
+
+# 判断正则表达式是否匹配
+import re
+re.match(r'\d{3}\-\d{3,8}$', '019-19823')
+# math()
+# 判断是否匹配
+# 成功返回一个Match对象
+# 失败返回None
+print(re.match(r'^\d{3}\s\d{3,8}$', '000 12345'))
+'''
+test = '用户输入的字符串'
+if re.match(r'正则表达式', test):
+    print('ok')
+else:
+    print('failed')
+'''
+
+# 切分字符串
+_str = 'ab  c'
+print(_str.split(' '))
+# 识别空格
+print(re.split(r'\s+', 'a   b, c'))
+# 识别逗号
+print(re.split(r'[\s\,]+', 'a, b  , c'))
+# 识别分号
+print(re.split(r'[\s\,\;]+', 'a;b;,c,'))
